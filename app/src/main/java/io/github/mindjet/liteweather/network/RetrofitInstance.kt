cf9a0028fad2d8a697f74e89d88a8841b2371dae
Mindjet <pearl920@outlook.com>
2017-10-22 23:40:36 +0800

    Update: add simple interceptor for Retrofit

--- app/src/main/java/io/github/mindjet/liteweather/network/RetrofitInstance.kt
+++ app/src/main/java/io/github/mindjet/liteweather/network/RetrofitInstance.kt
@@ -1,9 +1,12 @@
 package io.github.mindjet.liteweather.network
 
+import io.github.mindjet.library.log
 import io.github.mindjet.liteweather.BuildConfig
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Mindjet on 2017/10/16.
@@ -11,16 +14,26 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private val mRetrofit: Retrofit by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
                 .baseUrl(BuildConfig.BASE_URL)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build()
     }
 
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+            .connectTimeout(5000, TimeUnit.MILLISECONDS)
+            .addInterceptor {
+                val request = it.request()
+                log("---> ${request.method()}: ${request.url()}")
+                it.proceed(request)
+            }
+            .build()
+
     fun <T> getService(clazz: Class<T>): T {
-        return mRetrofit.create(clazz)
+        return retrofit.create(clazz)
     }
 
 }
