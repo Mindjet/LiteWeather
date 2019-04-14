3fad6bae8bfbc3980bbb4a25ef0ce6dab9e9c162
Mindjet <pearl920@outlook.com>
2019-04-14 18:41:58 +0800

    [-]: remove old shit

--- app/src/main/java/io/github/mindjet/liteweather/model/SearchResponse.kt
+++
@@ -1,6 +0,0 @@
-package io.github.mindjet.liteweather.model
-
-/**
- * Created by Mindjet on 2017/10/31.
- */
-data class SearchResponse(val basic: BasicV5, val status: String)
--- app/src/main/java/io/github/mindjet/liteweather/recycler_view/BaseViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package io.github.mindjet.liteweather.recycler_view
-
-import android.databinding.ViewDataBinding
-import android.support.v7.widget.RecyclerView
-
-/**
- * Created by Mindjet on 2017/10/19.
- */
-class BaseViewHolder<out B : ViewDataBinding>(val binding: B) : RecyclerView.ViewHolder(binding.root)
