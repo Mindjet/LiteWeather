80fe8f5fe21e8c14c12d41e4896321166221cf74
Mindjet <pearl920@outlook.com>
2019-04-28 22:55:22 +0800

    [+]: add swipe-to-refresh feature in home page

--- app/src/main/java/io/github/mindjet/liteweather/util/extension.kt
+++ app/src/main/java/io/github/mindjet/liteweather/util/extension.kt
@@ -3,10 +3,13 @@ package io.github.mindjet.liteweather.util
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+
+val handler = Handler()
 
 fun String.conditionColor(): Int {
     return ConditionHelper.getConditionColor(this)
@@ -29,6 +32,10 @@ inline infix fun <reified T> Activity.goto(clazz: Class<T>) {
 fun Activity.giveupFocus() {
     val inputManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputManager.hideSoftInputFromWindow(this.window.decorView.windowToken, 0)
+}
+
+fun Any.runDelay(millisecond: Long, body: () -> Unit) {
+    handler.postDelayed(body, millisecond)
 }
 
 //https://weatherapi.market.xiaomi.com/wtr-v3/weather/all?latitude=110&longitude=112&locationKey=weathercn%3A101010100&days=15&appKey=weather20151024&sign=zUFJoAR2ZVrDy1vF3D07&appVersion=87&isGlobal=false&modDevice=&locale=zh_cn
--- app/src/main/java/io/github/mindjet/liteweather/view/CityWeatherFragment.kt
+++ app/src/main/java/io/github/mindjet/liteweather/view/CityWeatherFragment.kt
@@ -2,6 +2,7 @@ package io.github.mindjet.liteweather.view
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -13,14 +14,17 @@ import io.github.mindjet.liteweather.constant.Constant
 import io.github.mindjet.liteweather.listener.ComListener
 import io.github.mindjet.liteweather.util.conditionColor
 import io.github.mindjet.liteweather.util.load
+import io.github.mindjet.liteweather.util.runDelay
 import io.github.mindjet.liteweather.util.turnTo
+import kotlinx.android.synthetic.main.fragment_city.view.*
 import kotlinx.android.synthetic.main.include_basic_info.view.*
 import kotlinx.android.synthetic.main.include_hourly_condition.view.*
 import kotlinx.android.synthetic.main.item_hourly_condition.view.*
 
-class CityWeatherFragment : Fragment() {
+class CityWeatherFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
     private var rootView: View? = null
+    private var cityName: String? = null
 
     companion object {
         fun newInstance(city: String): CityWeatherFragment {
@@ -46,12 +50,25 @@ class CityWeatherFragment : Fragment() {
 
     private fun createFreshView(inflater: LayoutInflater, container: ViewGroup?): View? {
         val view = inflater.inflate(R.layout.fragment_city, container, false)
-        val city = arguments?.getString(Constant.BUNDLE_CITY)
+        cityName = arguments?.getString(Constant.BUNDLE_CITY)
+        view.swipeRefreshLayout.setOnRefreshListener(this)
         startLoading(view)
+        getCityWeather(view, this::successLoading, this::failLoading)
+        return view
+    }
+
+    override fun onRefresh() {
+        startLoading(rootView)
+        runDelay(600) {
+            getCityWeather(rootView, this::successLoading, this::failLoading)
+        }
+    }
+
+    private fun getCityWeather(view: View?, onSuccess: (v: View?) -> Unit, onError: (v: View?) -> Unit) {
         HeWeather.getWeather(
-            context, city, ComListener.wholeWeather(
+            context, cityName, ComListener.wholeWeather(
                 {
-                    view.apply {
+                    view?.apply {
                         clWrapper.setBackgroundResource(it?.cond_code?.conditionColor()!!)
                         tvTemperature.text = resources.getString(R.string.degree_celsius_unit, it.tmp)
                         tvCondition.text = it.cond_txt
@@ -62,7 +79,7 @@ class CityWeatherFragment : Fragment() {
                     }
                 },
                 {
-                    view.apply {
+                    view?.apply {
                         recyclerView.adapter = HourlyConditionAdapter(it) {
                             tvTemperatureItem.text = resources.getString(R.string.degree_celsius_unit, it?.tmp)
                             tvPossibilityRainyItem.text = "${it?.pop}%"
@@ -72,39 +89,40 @@ class CityWeatherFragment : Fragment() {
                         recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
                         (recyclerView.adapter as HourlyConditionAdapter).notifyDataSetChanged()
                     }
-                },
-                { successLoading(view) },
-                { failLoading(view) }
+                }, { onSuccess.invoke(view) }, { onError.invoke(view) }
             )
         )
-        return view
     }
 
-    private fun failLoading(v: View) {
-        v.apply {
+    private fun failLoading(v: View?) {
+        v?.apply {
             includeLoadFailedBasicInfo turnTo View.GONE
             includeLoadFailedHourly turnTo View.GONE
             includeLoadingBasicInfo turnTo View.VISIBLE
             includeLoadingHourly turnTo View.VISIBLE
+            swipeRefreshLayout.isRefreshing = false
         }
     }
 
-    private fun successLoading(v: View) {
-        v.apply {
+    private fun successLoading(v: View?) {
+        v?.apply {
             includeLoadFailedBasicInfo turnTo View.GONE
             includeLoadFailedHourly turnTo View.GONE
             includeLoadingBasicInfo turnTo View.GONE
             includeLoadingHourly turnTo View.GONE
             clWrapper turnTo View.VISIBLE
+            recyclerView turnTo View.VISIBLE
+            swipeRefreshLayout.isRefreshing = false
         }
     }
 
-    private fun startLoading(v: View) {
-        v.apply {
+    private fun startLoading(v: View?) {
+        v?.apply {
             includeLoadFailedBasicInfo turnTo View.GONE
             includeLoadFailedHourly turnTo View.GONE
             includeLoadingBasicInfo turnTo View.VISIBLE
             includeLoadingHourly turnTo View.VISIBLE
+            recyclerView turnTo View.GONE
             clWrapper turnTo View.GONE
         }
     }
