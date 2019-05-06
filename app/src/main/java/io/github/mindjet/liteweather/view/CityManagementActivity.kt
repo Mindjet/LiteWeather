ad3ef0b2c375da640f1ec898d5cd77781fd1c6e6
Mindjet <pearl920@outlook.com>
2019-05-06 22:48:32 +0800

    [+]: add edit mode switch

--- app/src/main/java/io/github/mindjet/liteweather/view/CityManagementActivity.kt
+++ app/src/main/java/io/github/mindjet/liteweather/view/CityManagementActivity.kt
@@ -1,11 +1,14 @@
 package io.github.mindjet.liteweather.view
 
 import android.os.Bundle
+import android.view.View
 import io.github.mindjet.liteweather.R
 import io.github.mindjet.liteweather.adapter.CommonAdapter
 import io.github.mindjet.liteweather.model.City
 import io.github.mindjet.liteweather.util.CityHelper
 import io.github.mindjet.liteweather.util.setVerticalLinear
+import io.github.mindjet.liteweather.util.showToast
+import io.github.mindjet.liteweather.util.turnTo
 import kotlinx.android.synthetic.main.activity_city_management.*
 import kotlinx.android.synthetic.main.include_recycler_view.*
 import kotlinx.android.synthetic.main.item_city_management.view.*
@@ -13,6 +16,7 @@ import kotlinx.android.synthetic.main.item_city_management.view.*
 class CityManagementActivity : BaseAppCompatActivity() {
 
     private lateinit var adapter: CommonAdapter<City>
+    private var inEditMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,7 @@ class CityManagementActivity : BaseAppCompatActivity() {
                 vh.itemView.apply {
                     tvCity.text = model.name
                     tvCitySymbol.text = model.name[0].toString()
+                    ivReorder turnTo if (inEditMode) View.VISIBLE else View.GONE
                 }
             },
             layoutId = R.layout.item_city_management
@@ -34,6 +39,11 @@ class CityManagementActivity : BaseAppCompatActivity() {
 
         adapter.addAll(CityHelper.getPinnedCities(this)!!)
 
+        fab.setOnClickListener {
+            inEditMode = !inEditMode
+            showToast("editmode: $inEditMode")
+            adapter.notifyDataSetChanged()
+        }
     }
 
 }
