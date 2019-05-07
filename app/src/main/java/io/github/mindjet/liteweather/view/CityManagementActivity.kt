bc99b273a84b6a97eb1e3cf55f78ef003012855d
Mindjet <pearl920@outlook.com>
2019-05-07 23:52:10 +0800

    [+]: add checkbox for every pinned city

--- app/src/main/java/io/github/mindjet/liteweather/view/CityManagementActivity.kt
+++ app/src/main/java/io/github/mindjet/liteweather/view/CityManagementActivity.kt
@@ -1,13 +1,13 @@
 package io.github.mindjet.liteweather.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import io.github.mindjet.liteweather.R
 import io.github.mindjet.liteweather.adapter.CommonAdapter
 import io.github.mindjet.liteweather.model.City
 import io.github.mindjet.liteweather.util.CityHelper
 import io.github.mindjet.liteweather.util.setVerticalLinear
-import io.github.mindjet.liteweather.util.showToast
 import io.github.mindjet.liteweather.util.turnTo
 import kotlinx.android.synthetic.main.activity_city_management.*
 import kotlinx.android.synthetic.main.include_recycler_view.*
@@ -18,18 +18,23 @@ class CityManagementActivity : BaseAppCompatActivity() {
     private lateinit var adapter: CommonAdapter<City>
     private var inEditMode = false
 
+    private lateinit var checkStates: MutableList<Boolean>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_management)
-
         setSupportActionBar(toolbar)
 
+
+
         adapter = CommonAdapter(
-            onItemBound = { model, vh, _ ->
+            onItemBound = { model, vh, i ->
                 vh.itemView.apply {
                     tvCity.text = model.name
                     tvCitySymbol.text = model.name[0].toString()
                     ivReorder turnTo if (inEditMode) View.VISIBLE else View.GONE
+                    checkbox turnTo if (inEditMode) View.VISIBLE else View.GONE
+                    checkbox.setOnCheckedChangeListener { _, isChecked -> checkStates[i] = isChecked }
                 }
             },
             layoutId = R.layout.item_city_management
@@ -37,12 +42,21 @@ class CityManagementActivity : BaseAppCompatActivity() {
         recyclerView.setVerticalLinear(this)
         recyclerView.adapter = adapter
 
-        adapter.addAll(CityHelper.getPinnedCities(this)!!)
+        val cities = CityHelper.getPinnedCities(this)
+        adapter.addAll(cities!!)
 
+        //init checkStates, all true as they are chosen at the very first time
+        checkStates = cities.map { true } as MutableList<Boolean>
+
+        //floating action button
         fab.setOnClickListener {
             inEditMode = !inEditMode
-            showToast("editmode: $inEditMode")
+            fab.setImageResource(if (inEditMode) R.drawable.ic_done else R.drawable.ic_edit)
             adapter.notifyDataSetChanged()
+            if (!inEditMode) {
+                //TODO save states
+                checkStates.forEachIndexed { i, v -> Log.e("tag", "$i:$v") }
+            }
         }
     }
 
